d55b65aa64b6184172fd210301e28afc3258495e
Goraf <22941576+Goraf@users.noreply.github.com>
2018-06-10 20:37:47 +0200

    Add method to fetch data from iTunes Store ...
    * use fetch API
    * use config to set limit for results
    * display results in console

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+const config = {
+  server: "https://itunes.apple.com/search?",
+  searchResultLimit: 100,
+  songsPerPage: 9
+};
+
 class SearchApp {
   constructor() {
     [this.searchInput] = document.getElementsByClassName("search-form__input");
@@ -17,6 +23,33 @@ class SearchApp {
       this.searchInput.value = "";
       return;
     }
+
+    this.searchSong(searchQuery);
+    this.searchInput.value = "";
+  }
+
+  searchSong(searchQuery) {
+    this.callITunesApi(searchQuery)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw response.status;
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  callITunesApi(searchQuery) {
+    const term = searchQuery.replace(/\s+/g, "+");
+    const url = encodeURI(`${config.server}term=${term}&country=US&media=music&entity=song&limit=${config.searchResultLimit}`);
+
+    return fetch(url);
   }
 }
 
